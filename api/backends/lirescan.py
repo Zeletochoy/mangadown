7cfe4478c946db5e4686162152370dff5ecda119
Antoine Lecubin <alecubin@cogent.co.jp>
2018-11-22 17:18:56 +0900

    Fix lirescan

--- api/backends/lirescan.py
+++ api/backends/lirescan.py
@@ -30,11 +30,10 @@ def get_chapters(url):
     return chapters
 
 def download_chapter(url, path, loop):
-    os.makedirs(path, exist_ok=True)
     url = "http://www.lirescan.net" + url
     page = requests.get(url)
     soup = BeautifulSoup(page.text, "html.parser")
-    nav = soup.find(id="pagination")
+    nav = soup.find(class_="pagination")
     urls = []
     for elem in nav.find_all("a"):
         if elem.text.strip().isdigit():
@@ -49,4 +48,5 @@ def download_chapter(url, path, loop):
         if "__add" in fname.lower():
             continue # Crappy ad blocker
         urls[fname] = "http://www.lirescan.net" + link
+    os.makedirs(path, exist_ok=True)
     utils.download_urls(urls, path, loop)
